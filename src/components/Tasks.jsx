f6040c5d7435c3f74a5e20cef8acd625181793b8
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-15 11:41:26 -0300

    refactor: add TasksSeparator component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -4,6 +4,7 @@ import TrashIcon from '../assets/icons/trash.svg?react'
 import SunIcon from '../assets/icons/sun.svg?react'
 import CloudSunIcon from '../assets/icons/cloud-sun.svg?react'
 import MoonIcon from '../assets/icons/moon.svg?react'
+import TasksSeparator from './TasksSeparator'
 
 const Tasks = () => {
   return (
@@ -28,26 +29,16 @@ const Tasks = () => {
       </div>
 
       <div className="rounded-xl bg-white p-6">
-        {/* Manhã */}
         <div className="space-y-3">
-          <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
-            <SunIcon />
-            <p className="text-sm text-[#9A9C9F]">Manhã</p>
-          </div>
+          <TasksSeparator title="Manhã" icon={<SunIcon />} />
         </div>
-        {/* Tarde */}
+
         <div className="my-3 space-y-6">
-          <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
-            <CloudSunIcon />
-            <p className="text-sm text-[#9A9C9F]">Tarde</p>
-          </div>
+          <TasksSeparator title="Tarde" icon={<CloudSunIcon />} />
         </div>
-        {/* Noite */}
+
         <div className="space-y-3">
-          <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
-            <MoonIcon />
-            <p className="text-sm text-[#9A9C9F]">Noite</p>
-          </div>
+          <TasksSeparator title="Noite" icon={<MoonIcon />} />
         </div>
       </div>
     </div>
