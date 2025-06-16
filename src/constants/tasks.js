08bf88cde7c0d0ef779445bdaa68d18ab69ce44b
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-16 16:02:06 -0300

    feat: ensure tasks are listed on Tasks component

---
+++ src/constants/tasks.js
@@ -0,0 +1,46 @@
+const TASKS = [
+  {
+    id: 1,
+    title: 'Academia',
+    description: 'Treinar costas',
+    time: 'morning',
+    status: 'done',
+  },
+  {
+    id: 2,
+    title: 'Água',
+    description: 'Beber 3L de água',
+    time: 'morning',
+    status: 'done',
+  },
+  {
+    id: 3,
+    title: 'Faculdade',
+    description: 'Estudar para a prova da faculdade',
+    time: 'afternoon',
+    status: 'in_progress',
+  },
+  {
+    id: 4,
+    title: 'Estudar',
+    description: 'Revisar conteúdos da matéria',
+    time: 'afternoon',
+    status: 'in_progress',
+  },
+  {
+    id: 5,
+    title: 'Compras',
+    description: 'Fazer compras no shopping',
+    time: 'evening',
+    status: 'not_started',
+  },
+  {
+    id: 6,
+    title: 'Descansar',
+    description: 'Assistir um filme',
+    time: 'evening',
+    status: 'not_started',
+  },
+]
+
+export default TASKS
