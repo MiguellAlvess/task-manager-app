2334df55f0b76a27d1b7a637c89b39079d8d15f2
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-30 20:12:45 -0300

    feat: add home and tasks page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import SidebarButton from './SidebarButton'
 
 const Sidebar = () => {
   return (
-    <div className="h-screen w-72 bg-white">
+    <div className="h-screen w-72 min-w-72 bg-white">
       <div className="space-y-4 px-8 py-6">
         <h1 className="mb-4 text-xl font-semibold text-brand-primary">
           Task Manager
@@ -14,11 +14,11 @@ const Sidebar = () => {
         </p>
       </div>
       <div className="flex flex-col gap-2 p-2">
-        <SidebarButton color="unselected">
+        <SidebarButton href="/" color="unselected">
           <HomeIcon />
           Início
         </SidebarButton>
-        <SidebarButton color="selected">
+        <SidebarButton href="/tasks" color="selected">
           <TasksIcon /> Minhas tarefas
         </SidebarButton>
       </div>
